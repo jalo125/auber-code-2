14e762b995d40eebc0565775e08d19c355486ca6
Muhidin <muhidinmo@hotmail.com>
2020-10-29 16:29:12 +0000

    first commit

---
+++ Hello/core/src/com/mygdx/game/HelloWorld.java
@@ -0,0 +1,42 @@
+package com.mygdx.game;
+
+import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+
+public class HelloWorld extends ApplicationAdapter {
+	SpriteBatch batch;
+	Texture img;
+	Sprite sprite;
+	
+	@Override
+	public void create () {
+		batch = new SpriteBatch();
+		img = new Texture("pictureHelloWorld.png");
+		sprite = new Sprite(img);
+		sprite.setPosition(
+				Gdx.graphics.getWidth()/2 - sprite.getWidth()/2,
+				Gdx.graphics.getHeight()/2 - sprite.getHeight()/2
+		);
+		sprite.setRotation(90f);
+	}
+
+	@Override
+	public void render () {
+		Gdx.gl.glClearColor(0, 0, 0, 1);
+		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+		batch.begin();
+		batch.draw(sprite, sprite.getX(), sprite.getY(), sprite.getWidth()/2, sprite.getHeight()/2,
+				sprite.getWidth(), sprite.getHeight(), sprite.getScaleX(), sprite.getScaleY(), sprite.getRotation());
+		batch.end();
+	}
+	
+	@Override
+	public void dispose () {
+		batch.dispose();
+		img.dispose();
+	}
+}
